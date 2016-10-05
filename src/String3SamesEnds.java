fd4237ea1f770df3784ded2cf75996afc6553a79
miservilla <miservilla@gmail.com>
2016-10-04 20:36:02 -0600

    Initial commit

---
+++ src/String3SamesEnds.java
@@ -0,0 +1,20 @@
+import com.sun.org.apache.xpath.internal.Arg;
+
+/**
+ * Created by miser on 10/4/2016.
+ */
+public class String3SamesEnds {
+
+    public static String sameEnds(String string){
+        String result = "";
+        for (int i = 0; i < string.length(); i++) {
+            if (string.charAt(i) == string.charAt(string.length()-i)){
+                result.
+            }
+        }
+    }
+
+    public static void main(String[] args){
+
+    }
+}
